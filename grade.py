80bf15d5aac85a74777c3a839d5b76fe936cf7ee
Hrudya712 <66232566+Hrudya712@users.noreply.github.com>
2020-08-23 16:45:16 +0530

    Add files via upload

---
+++ grade.py
@@ -0,0 +1,10 @@
+a=float(input("enter marks of maths"))
+b=float(input("enter marks of physics"))
+c=float(input("enter marks of chemistry"))
+d=float(input("enter marks of data structures"))
+e=float(input("enter marks of english"))
+sum=(a+b+c+d+e)/5
+if(sum>=30):
+    print("pass")
+else:
+    print("fail")
